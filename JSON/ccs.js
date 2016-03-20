bcbeb9c922e734e2fba4c33e7a914d7904ab2eba
Igorce9 <uchihagammes@gmail.com>
2016-03-20 17:57:36 -0300

    Update and rename ccs.json to ccs.js

--- JSON/ccs.js
+++ JSON/ccs.js
@@ -1,10 +1,13 @@
-{
+window.ipw = { 
   "customRooms" : {
     "edtentertainment" : {
       "title" : "EDTPlugWoot" 
     },
     "trap-core" : {
       "title" : "CorePlugWoot"
+    },
+    "chill-chillstep-ambient-music" : {
+      "title" : "ChillPlugWoot"
     }
   }
-}
+};
