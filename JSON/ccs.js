371898db442c788acb2121f94d75774af4ce9775
Igorce9 <uchihagammes@gmail.com>
2016-06-11 15:12:34 -0300

    Update ccs.js

--- JSON/ccs.js
+++ JSON/ccs.js
@@ -3,6 +3,9 @@ window.ipw = {
     "edtentertainment" : {
       "title" : "EDTPlugWoot",
       "background" : "https://i.imgur.com/QKspQYZ.jpg"
+    },
+    "world-of-pop" : {
+      "title" : "WOPPlugWoot"
     }
   }
 };
